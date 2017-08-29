588271ee5d7e1b2827498721fbf43453762601e8
rmit-s3607050-rei-ito <s3607050@student.rmit.edu.au>
2017-08-29 17:15:31 +1000

    bst done (hopefully)

--- src/BstMultiset.java
+++ src/BstMultiset.java
@@ -33,160 +33,65 @@ public class BstMultiset<T> extends Multiset<T>
   protected Node mRoot;
 
   public BstMultiset() {
-    this.mRoot = null;
+    mRoot = null;
   } // end of BstMultiset()
 
   public void add(T item) {
-    Node newItem = new Node((String)item);
-
-    if (mRoot == null) {
-      mRoot = newItem;
-      return;
-    }
-
-    Node current = mRoot;
-    Node parent = null;
-
-    while (true) {
-      parent = current;
-      if (current.getKey().compareTo((String)item) < 0) {
-        current = current.getLeft();
-        if (current == null) {
-          parent.setLeft(newItem);
-          return;
-        }
-      }
-      else if (current.getKey().compareTo((String)item) > 0 ) {
-        current = current.getRight();
-        if (current == null) {
-          parent.setRight(newItem);
-          return;
-        }
-      }
-      else {
-        current.incCount();
-        return;
-      }
-    }
+    mRoot = insert(mRoot, (String)item);
   } // end of add()
 
-  private Node searchNode(T item) {
-    Node current = mRoot;
-    while (current != null) {
-      if (current.getKey().compareTo((String)item) == 0) {
-        return current;
-      }
-      else if (current.getKey().compareTo((String)item) < 0) {
-        current = current.getLeft();
-      }
-      else {
-        current = current.getRight();
-      }
+  private Node insert(Node root, String key) {
+    // check if root is empty
+    if (root == null) {
+      root = new Node(key);
     }
-    return null;
+    else if (root.getKey().compareTo(key) < 0) {
+      root.setLeft(insert(root.getLeft(), key));
+    }
+    else if (root.getKey().compareTo(key) > 0) {
+      root.setRight(insert(root.getRight(), key));
+    }
+    else {
+      root.incCount();
+    }
+
+    return root;
+  } // end of insert()
+
+  private Node searchNode(Node root, String key) {
+    if (root == null)
+      return root;
+
+    if (root.getKey().compareTo(key) < 0)
+      return searchNode(root.getLeft(), key);
+    else if (root.getKey().compareTo(key) > 0)
+      return searchNode(root.getRight(), key);
+
+    return root;
   }
 
   public int search(T item) {
-    Node sNode = searchNode(item);
-    if (sNode == null) {
+    Node searched = searchNode(mRoot, (String)item);
+    if (searched == null)
       return 0;
-    }
-    else {
-      return sNode.getCount();
-    }
+
+    return searched.getCount();
   } // end of search()
 
 
   public void removeOne(T item) {
-    Node found = searchNode(item);
-    if (found.getCount() > 1) {
-      found.decCount();
-    }
-    else {
+    Node searched = searchNode(mRoot, (String)item);
+    if (searched == null)
+      return;
+
+    if (searched.getCount() > 1)
+      searched.decCount();
+    else
       removeAll(item);
-    }
   } // end of removeOne()
 
-  //private Node getSuccessor(Node delNode) {
-  //  Node successor = null;
-  //  Node successorParent = null;
-  //  Node current = delNode.getRight();
-
-  //  while (current != null) {
-  //    successorParent = successor;
-  //    successor = current;
-  //    current = current.getLeft();
-  //  }
-
-  //  if (successor != delNode.getRight()) {
-  //    successorParent.setLeft(successor.getRight());
-  //    successor.setRight(delNode.getRight());
-  //  }
-
-  //  return successor;
-  //}
-
-  //public void removeAll(T item) {
-  //  Node parent = mRoot;
-  //  Node current = mRoot;
-  //  boolean isLeftChild = false;
-
-  //  while (current.getKey() != (String)item) {
-  //    parent = current;
-  //    if (current.getKey().compareTo((String)item) < 0) {
-  //      isLeftChild = true;
-  //      current = current.getLeft();
-  //    }
-  //    else {
-  //      isLeftChild = false;
-  //      current = current.getRight();
-  //    }
-
-  //    if (current == null)
-  //      return;
-  //  }
-
-  //  if (current.getLeft() == null && current.getRight() == null) {
-  //    if (current == mRoot)
-  //      mRoot = null;
-  //    if (isLeftChild)
-  //      parent.setLeft(null);
-  //    else
-  //      parent.setRight(null);
-  //  }
-  //  else if (current.getRight() == null) {
-  //    if (current == mRoot)
-  //      mRoot = current.getLeft();
-  //    else if (isLeftChild)
-  //      parent.setLeft(current.getLeft());
-  //    else
-  //      parent.setRight(current.getLeft());
-  //  }
-  //  else if (current.getLeft() == null) {
-  //    if (current == mRoot)
-  //      mRoot = current.getRight();
-  //    else if (isLeftChild)
-  //      parent.setLeft(current.getRight());
-  //    else
-  //      parent.setRight(current.getRight());
-  //  }
-  //  else if (current.getLeft() != null && current.getRight() != null) {
-  //    Node successor = getSuccessor(current);
-
-  //    if (current == mRoot)
-  //      mRoot = successor;
-  //    else if (isLeftChild)
-  //      parent.setLeft(successor);
-  //    else
-  //      parent.setRight(successor);
-
-  //    successor.setLeft(current.getLeft());
-  //  }
-  //} // end of removeAll()
-
   public void removeAll(T item) {
-    System.out.println("called");
-    deleteNode(mRoot, (String)item);
+    mRoot = deleteNode(mRoot, (String)item);
   }
 
   private Node minElement(Node root) {
@@ -196,24 +101,25 @@ public class BstMultiset<T> extends Multiset<T>
       return minElement(root.left);
   }
 
-  private Node deleteNode(Node root, String value) {
+  private Node deleteNode(Node root, String key) {
     if (root == null)
       return null;
 
-    if (root.getKey().compareTo(value) < 0)
-      root.setLeft(deleteNode(root.getLeft(), value));
-    else if (root.getKey().compareTo(value) > 0)
-      root.setRight(deleteNode(root.getRight(), value));
+    if (root.getKey().compareTo(key) < 0)
+      root.setLeft(deleteNode(root.getLeft(), key));
+    else if (root.getKey().compareTo(key) > 0)
+      root.setRight(deleteNode(root.getRight(), key));
     else {
       // if nodeToBeDeleted have both children
       if (root.getLeft() != null && root.getRight() != null) {
-        Node temp = root;
+        //Node temp = root;
         // Finding minimum element from right
-        Node minNodeForRight = minElement(temp.getRight());
+        Node minNodeForRight = minElement(root.getRight());
         // Replacing current node with minimum node from right subtree
         root.setKey(minNodeForRight.getKey());
+        root.setCount(minNodeForRight.getCount());
         // Deleting minimum node from right now
-        deleteNode(root.getRight(), minNodeForRight.getKey());
+        root.setRight(deleteNode(root.getRight(), minNodeForRight.getKey()));
       }
       // if nodeToBeDeleted has only left child
       else if (root.getLeft() != null)
